8c461d49d026d59b3e0898f9ea8c91e3376fb466
Daniel Sanchez <jdanielsancheza@gmail.com>
2023-11-27 16:42:02 -0500

    AudioManager.cs

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using DG.Tweening;
+using UnityEngine.Serialization;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public AudioMixerSnapshot snapshotIn;
+    public AudioMixerSnapshot snapshotOut;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        SetSnapshot(snapshotIn);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0;
+        audioSource.DOFade(0.5f, 1);
+    }
+
+    [ContextMenu("IN")]
+    [Yarn.Unity.YarnCommand("setsnapshotIN")]
+    public void SetSnapshotIn()
+    {
+        SetSnapshot(snapshotIn);
+    }
+
+    [ContextMenu("OUT")]
+    public void SetSnapshotOut()
+    {
+        SetSnapshot(snapshotOut);
+    }
+
+    private void SetSnapshot(AudioMixerSnapshot snapshot)
+    {
+        if (snapshot != null)
+        {
+            snapshot.TransitionTo(2f);
+        }
+    }
+}
