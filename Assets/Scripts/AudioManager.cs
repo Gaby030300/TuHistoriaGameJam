4ebe95e5faab1fd7535eebfa264dacb385f8abae
Daniel Sanchez <jdanielsancheza@gmail.com>
2023-11-27 19:24:41 -0500

    Mixer tweak

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -31,6 +31,7 @@ public class AudioManager : MonoBehaviour
 
     private void Start()
     {
+        SetSnapshotOut();
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = 0;
         audioSource.DOFade(0.5f, 1);
